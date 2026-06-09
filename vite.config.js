62216047823e615327a08a3bf0fd5de58a6dbb18
Akshay <720akshaykumar@gmail.com>
2026-06-09 17:55:16 +0530

    this is

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,18 @@ export default defineConfig({
     watch: {
       usePolling: true
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 1600, // Chunk size ki warning limit ko badhane ke liye
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Ye line saari badi external libraries (react-icons, redux etc.) ko alag-alag chhote chunks me tod degi
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 })
