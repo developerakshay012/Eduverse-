5ff60ae2b264f855a6e4a1c10714a5e0b525a2dd
Akshay <720akshaykumar@gmail.com>
2026-06-09 16:36:12 +0530

    fix: restore missing files and apply correct casing

--- src/Components/Core/HomePage/HighlightText.jsx
+++ src/Components/Core/HomePage/HighlightText.jsx
@@ -5,8 +5,9 @@ function HighlightText({text}) {
     <span className='bg-linear-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] text-transparent bg-clip-text font-bold'>
         {" "}
         {text}
+        {/* /i have changes  */}
     </span>
   )
 }
-  
+
 export default HighlightText
